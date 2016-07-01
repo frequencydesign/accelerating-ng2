74adda5c11f3088ced45500f76294dff6223f687
unknown <mahdi.tehrani@mtehraniW520.primitivelogic.com>
2016-07-01 11:44:41 -0700

    finished 3.4 video. Split out html template and css styles to seperate files and re-imported themby adding references to them using decorator properties which point to the file locations

--- app/car-parts.component.ts
+++ app/car-parts.component.ts
@@ -3,27 +3,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "car-parts",
-    template: `
-  <p>There are {{totalCarParts()}} total parts in stock.</p>
-  <ol>
-    <li *ngFor="let carPart of carParts">
-      <h2>{{carPart.name | uppercase}}</h2>
-      <p class="description">{{carPart.description}}</p>
-      <p class="price">{{carPart.price | currency:'EUR':true}}</p>
-      <p *ngIf="carPart.inStock > 0">{{carPart.inStock}} in Stock</p>
-      <p *ngIf="carPart.inStock === 0">Out of Stock</p>
-    </li>
-  </ol>
-  `,
-    styles: [`
-        .description {
-            color: #444;
-            font-size: small;
-        }
-        .price {
-            font-weight: bold;
-         }
-    `]
+    templateUrl: "app/car-parts.component.html",
+    styleUrls: ["app/car-parts.component.css"]
 })
 
 export class CarPartsComponent {
